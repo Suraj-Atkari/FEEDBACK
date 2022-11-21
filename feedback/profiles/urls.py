1124c478658388d13bdf4f35096cd1f07a425b6c
Suraj Atkari <surajatkari@yahoo.com>
2022-11-21 20:36:34 +0530

    added urls to settings to make sure the app link to the root

--- feedback/profiles/urls.py
+++ feedback/profiles/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("/profiles", views.CreateProfileView.as_view())
+    path("profiles", views.CreateProfileView.as_view())
 ]
