1b67663bc51f622cdd91732bbded9ea851bc7a3d
Suraj Atkari <surajatkari@yahoo.com>
2022-11-22 16:41:35 +0530

    added CreatView class to avoid all the manual work

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -3,25 +3,35 @@ from django.views import View
 from django.http import HttpResponseRedirect
 from .form import ProfileForm
 from .models import UserProfile
+from django.views.generic.edit import CreateView
 
 # Create your views here.
 
+# Bellow CreaView will help us avoid all the things written bellow in #
 
-class CreateProfileView(View):
-    def get(self, request):
-        form = ProfileForm()
-        return render(request, "profiles/create_profile.html", {
-            "form": form
-        })
 
-    def post(self, request):
-        submitted_form = ProfileForm(request.POST, request.FILES)
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"
 
-        if submitted_form.is_valid():
-            profile = UserProfile(image=request.FILES["user_image"])
-            profile.save()
-            return HttpResponseRedirect("/profiles")
 
-        return render(request, "profiles/create_profile.html", {
-            "form": submitted_form
-        })
+# class CreateProfileView(View):
+#     def get(self, request):
+#         form = ProfileForm()
+#         return render(request, "profiles/create_profile.html", {
+#             "form": form
+#         })
+
+#     def post(self, request):
+#         submitted_form = ProfileForm(request.POST, request.FILES)
+
+#         if submitted_form.is_valid():
+#             profile = UserProfile(image=request.FILES["user_image"])
+#             profile.save()
+#             return HttpResponseRedirect("/profiles")
+
+#         return render(request, "profiles/create_profile.html", {
+#             "form": submitted_form
+#         })
